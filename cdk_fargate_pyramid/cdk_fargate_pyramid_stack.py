b3c98093962792c3976b9a2ad55401baf3b4fc85
Keenan Graham <ksgraham@stanford.edu>
2022-03-17 16:43:50 -0700

    Try add database

--- cdk_fargate_pyramid/cdk_fargate_pyramid_stack.py
+++ cdk_fargate_pyramid/cdk_fargate_pyramid_stack.py
@@ -1,11 +1,20 @@
 import aws_cdk as cdk
 
 from aws_cdk.aws_ec2 import SecurityGroup
+from aws_cdk.aws_ec2 import InstanceType
+from aws_cdk.aws_ec2 import InstanceClass
+from aws_cdk.aws_ec2 import InstanceSize
+from aws_cdk.aws_ec2 import SubnetSelection
+from aws_cdk.aws_ec2 import SubnetType
 
 from aws_cdk.aws_ecs import ContainerImage
 
 from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedFargateService
 from aws_cdk.aws_ecs_patterns import ApplicationLoadBalancedTaskImageOptions
+
+from aws_cdk.aws_rds import DatabaseInstance
+from aws_cdk.aws_rds import DatabaseInstanceEngine
+from aws_cdk.aws_rds import PostgresEngineVersion
 
 from constructs import Construct
 
@@ -26,6 +35,33 @@ class CdkFargatePyramidStack(cdk.Stack):
             'sg-022ea667',
             mutable=False
         )
+        engine = DatabaseInstanceEngine.postgres(
+            version=PostgresEngineVersion.VER_14_1
+        )
+        database = DatabaseInstance(
+            self,
+            'TestFargatePyramidAppPostgres',
+            database_name='igvfd',
+            engine=engine,
+            instance_type=InstanceType.of(
+                InstanceClass.BURSTABLE3,
+                InstanceSize.MEDIUM,
+            ),
+            vpc=vpcs.default_vpc,
+            vpc_subnets=SubnetSelection(
+                subnet_type=SubnetType.PUBLIC,
+            ),
+            allocated_storage=10,
+            max_allocated_storage=20,
+            security_groups=[
+                SecurityGroup.from_security_group_id(
+                    self,
+                    'encd_sg',
+                    'sg-022ea667',
+                    mutable=False
+                ),
+            ],
+        )
         application_image = ContainerImage.from_asset(
             'cdk_fargate_pyramid/pyramid/'
         )
@@ -37,6 +73,9 @@ class CdkFargatePyramidStack(cdk.Stack):
             desired_count=1,
             task_image_options=ApplicationLoadBalancedTaskImageOptions(
                 image=application_image,
+                environment={
+                    'SQLALCHEMY_URL': database.instance_endpoint,
+                }
             ),
             memory_limit_mib=2048,
             public_load_balancer=True,
