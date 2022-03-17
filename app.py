53fafe35645e41bcb62cee8ab824c7605f4caeaa
Keenan Graham <ksgraham@stanford.edu>
2022-03-17 15:51:27 -0700

    Add env

--- app.py
+++ app.py
@@ -1,28 +1,16 @@
-#!/usr/bin/env python3
-import os
-
 import aws_cdk as cdk
 
 from cdk_fargate_pyramid.cdk_fargate_pyramid_stack import CdkFargatePyramidStack
 
+from shared_infrastructure.cherry_lab.environments import US_WEST_2
+
 
 app = cdk.App()
-CdkFargatePyramidStack(app, "CdkFargatePyramidStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
-    )
+CdkFargatePyramidStack(
+    app,
+    'CdkFargatePyramidStack',
+    env=US_WEST_2,
+)
 
 app.synth()
--- cdk_fargate_pyramid/cdk_fargate_pyramid_stack.py
+++ cdk_fargate_pyramid/cdk_fargate_pyramid_stack.py
@@ -1,19 +1,9 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+import aws_cdk as cdk
+
 from constructs import Construct
 
-class CdkFargatePyramidStack(Stack):
+
+class CdkFargatePyramidStack(cdk.Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        # The code that defines your stack goes here
-
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkFargatePyramidQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
